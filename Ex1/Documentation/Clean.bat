80a16951301e6753700cb7b520434b52e8f6f45b
katci <33861931+katci@users.noreply.github.com>
2019-10-26 12:17:35 +0200

    Homework Layout Updated

---
+++ Ex1/Documentation/Clean.bat
@@ -0,0 +1,39 @@
+del *.aux
+
+del *.log
+
+del *.lof
+
+del *.bak
+
+del *.loa
+
+del *.lot
+
+del *.blg
+
+del *.dvi
+
+del *.out
+
+del *.brf
+
+del *.thm
+
+del *.toc
+
+del *.ps
+
+del *.idx
+
+del *.ilg
+
+del *.ind
+
+del *.tps
+
+del *.gz
+
+del *.xml
+
+del *.sav
---
+++ Ex1/Documentation/TexStructure/Clean.bat
@@ -0,0 +1,45 @@
+del *.aux
+
+del *.log
+
+del *.lof
+
+del *.bak
+
+del *.loa
+
+del *.log
+
+del *.lot
+
+del *.bbl
+
+del *.blg
+
+del *.dvi
+
+del *.out
+
+del *.brf
+
+del *.thm
+
+del *.toc
+
+del *.ps
+
+del *.idx
+
+del *.ilg
+
+del *.ind
+
+del *.tps
+
+del *.gz
+
+del *.xml
+
+del *.sav
+
+del *.bcf
