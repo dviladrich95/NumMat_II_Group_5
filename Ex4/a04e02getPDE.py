4a0c65f852a1eeef33b14517a524fe42ec40a96d
dviladrich95 <dviladrich95@gmail.com>
2019-11-15 16:43:47 +0100

    solved index problems

--- Ex4/a04e02getPDE.py
+++ Ex4/a04e02getPDE.py
@@ -13,7 +13,7 @@ def analytical_f(x):
 def get_x_list(N):
 	x=[]
 	while len(x)!=N:
-		x=np.random.uniform(size=N-2)
+		x=np.random.uniform(0,N,size=N-2)
 		print(x)
 		x=np.insert(x,0,0)
 		print(x)
@@ -23,6 +23,10 @@ def get_x_list(N):
 		print(x)
 	return x
 
+
+
+
+
 def a04ex02getPDE(x,f,consts,flag):
 	
 	a=consts[0]
@@ -30,53 +34,58 @@ def a04ex02getPDE(x,f,consts,flag):
 	c=consts[2]
 	alpha=consts[3]
 	beta=consts[4]
-	
+	print(x,'x')
 	h=np.diff(x)
-	print(h,'h')
+	h=[x[i]-x[i-1] for i in range(1,len(x))]
+
+	f_dd_m1= lambda i: 2/(h[i]*(h[i]+h[i+1]))
+	f_dd_0= lambda i: -2/(h[i]*h[i+1])
+	f_dd_p1= lambda i: 2/(h[i+1]*(h[i]+h[i+1]))
+
+	f_d_1= lambda i: -1/(h[i]+h[i+1])
+	f_d_2= lambda i: 1/(h[i]+h[i+1])
+
 	N=len(h)-1
 
-	dd_m1=[2/(h[i]*(h[i]+h[i+1])) for i in range(N-1)]
+	dd_m1=[f_dd_m1(i) for i in range(1,N)]
 	print(dd_m1,'dd_m1')
-	dd_0=[-2/(h[i]*h[i+1]) for i in range(N)]
+	dd_0=[f_dd_0(i) for i in range(N)]
 	print(dd_0,'dd_0')
-	dd_p1=[2/(h[i+1]*(h[i]+h[i+1])) for i in range(N-1)]
+	dd_p1=[f_dd_p1(i) for i in range(N-1)]
 	print(dd_p1,'dd_p1')
 	dd=np.diag(dd_m1,k=-1)+np.diag(dd_0,k=0)+np.diag(dd_p1,k=1)
-	print(dd)
+	print(dd,'dd')
 	i=sps.eye(N)
 
 	if flag=='-':
-		d_1=[-1/h[i] for i in range(N-1)]
-		print(d_1)
-		d_2=[1/h[i] for i in range(N)]
+		d_1=[f_d_1(i) for i in range(1,N)]
+		print(d_1,'d_1')
+		d_2=[f_d_2(i) for i in range(N)]
 
 		d=np.diag(d_1,k=-1)+np.diag(d_2,k=0)
 	elif flag=='+':
-		d_1=[-1/h[i+1] for i in range(N)]
-		d_2=[1/h[i+1] for i in range(N-1)]
+		d_1=[f_d_1(i) for i in range(N)]
+		d_2=[f_d_2(i) for i in range(N-1)]
 		d=np.diag(d_1,k=0)+np.diag(d_2,k=1)
 	elif flag=='0':
-		d_1=[-1/(h[i]+h[i+1]) for i in range(N-1)]
-		print(d_1,'d_1')
-		d_2=[1/(h[i]+h[i+1]) for i in range(N-1)]
-		print(d_2,'d_2')
+		d_1=[f_d_1(i) for i in range(1,N)]
+		d_2=[f_d_2(i) for i in range(N-1)]
 		d=np.diag(d_1,k=-1)+np.diag(d_2,k=1)
 		print(d,'d')
 	l=-a*dd+b*d+c*i
 	print(l)
 
 	if flag=='-':
-		f[0]=f[0]+(a*dd_m1[0]-b*d_1[0])*alpha
-		f[-1]=f[-1]+a*dd_p1[-1]*beta
+		f[0]=f[0]+(a*f_dd_m1(0)-b*f_d_1(0))*alpha
+		f[-1]=f[-1]+a*f_dd_p1(N-1)*beta
 	elif flag=='+':
-		f[0]=f[0]+a*dd_m1[0]
-		f[-1]=f[-1]+(a*dd_p1[0]-b*d_2[-1])*beta
+		f[0]=f[0]+a*f_dd_m1(0)
+		f[-1]=f[-1]+(a*f_dd_p1(N-1)-b*f_d_2(N-1))*beta
 	elif flag=='0':
-		f[0]=f[0]+(a*dd_m1[0]-b*d_1[0])*alpha
-		f[-1]=f[-1]+(a*dd_p1[0]-b*d_2[-1])*beta
+		f[0]=f[0]+(a*f_dd_m1(0)-b*f_d_1(0))*alpha
+		f[-1]=f[-1]+(a*f_dd_p1(N-1)-b*f_d_2(N-1))*beta
 	
 	return l,f
-
 
 def a04ex02solve(x,f,consts,flag):
 
@@ -91,19 +100,21 @@ def a04ex02solve(x,f,consts,flag):
 	u_h=np.append(u_h,beta)
 	return u_h
 
-N=2
+N=10
 #consts=[a,b,c,alpha,beta]
 consts=[1,-4,1,1,2]
+#consts=[1,0,-1,0,0]
 #x=get_x_list(N)
 x=np.linspace(0,1,num=N+2)
 ax=sns.scatterplot(x,analytical_u(x))
-for noise in np.linspace(0,0.0,num=2):
-	x=np.linspace(0,1,num=N+2)**2+noise/2*np.random.uniform(-1/(N+2),1/(N+2),N+2)
+for noise in np.linspace(0,1,num=2):
+	x=np.linspace(0,1,num=N+2)+noise/2*np.random.uniform(-1/(N+2),1/(N+2),N+2)
 	x[0]=0
 	x[-1]=1
 	f=analytical_f(x)[1:-1]
-	flag='-'
+	#f=np.ones(N)
+	flag='0'
 	u_h=a04ex02solve(x,f,consts,flag)
-	sns.scatterplot(x,u_h)
+	ax=sns.scatterplot(x,u_h)
 ax.set(xlabel='x', ylabel='u_h')
 plt.show()
