93be61c76a10b50a10fdd1ba1768f0b1efe65428
Riccardo Parise <riccardo@parise.space>
2020-01-21 15:56:16 +0100

    housearea done

--- Ex9/housearea.py
+++ Ex9/housearea.py
@@ -7,15 +7,19 @@ import seaborn as sns
 import pandas as pd
 from readtria import *
 
-
 def main():
     [x, y, npoint, nelement, e2p, idp, ide] = readtria("./meshes/haus")
+    # initialize area
+    area = 0
+    # add each element
     for k in range(nelement):
-        e2 = e2p[k]
-        [Fdet, Finv] = generatetransformation2D(k, e2, x, y)
+        [Fdet, Finv] = generatetransformation2D(k, e2p, x, y)
+        area = area+Fdet/2  # have to divide by two. area of reference element is 1/2
+    print(area)
     return
 
-def generatetransformation2D(k, e2, x, y):
+def generatetransformation2D(k, e2p, x, y):
+    e2 = e2p[k]
     zi1 = x[e2][0]
     zj1 = x[e2][1]
     zk1 = x[e2][2]
@@ -23,12 +27,10 @@ def generatetransformation2D(k, e2, x, y):
     zj2 = y[e2][1]
     zk2 = y[e2][2]
 
-    A = [[zj1-zi1, zk1-zi1],[zj2-zi2, zk2-zi2]]
+    Fdet = np.linalg.det([[zj1-zi1, zk1-zi1],[zj2-zi2, zk2-zi2]])
+    Finv = 1/Fdet
 
-    print(A)
-    Fdet = np.linalg.det(T)
-
-    return
+    return Fdet, Finv
 
 
 if __name__ == '__main__':
