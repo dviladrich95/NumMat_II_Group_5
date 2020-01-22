09542cd26579dbc379948277c77c83b0838caa94
Riccardo Parise <riccardo@parise.space>
2020-01-22 22:17:24 +0100

    changed the mref empirically and got a nicer plot (:
    plus cosmetic changes and plot exact solution

--- Ex9/checksolution.py
+++ Ex9/checksolution.py
@@ -3,7 +3,6 @@ from scipy import sparse
 from scipy.sparse.linalg import spsolve
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
 from readtria import readtria
 
 
@@ -12,50 +11,54 @@ from readtria import readtria
 # def localstiff2D(Fdet,Finv):
 # def localmass2D(Fdet):
 
-def generatetransformation2D(k,e2,x,y):
-    i,j,k=e2[k,:]
-    zi1,zj1,zk1=x[i],x[j],x[k]
-    zi2,zj2,zk2=y[i],y[j],y[k]
+def generatetransformation2D(k, e2, x, y):
+    i, j, k = e2[k, :]
+    zi1, zj1, zk1 = x[i], x[j], x[k]
+    zi2, zj2, zk2 = y[i], y[j], y[k]
 
-    F_l=np.array([[zj1-zi1,zk1-zi1],[zj2-zi2,zk2-zi2]])
+    F_l = np.array([[zj1 - zi1, zk1 - zi1], [zj2 - zi2, zk2 - zi2]])
 
-    Fdet=np.linalg.det(F_l)
+    Fdet = np.linalg.det(F_l)
 
-    Finv=np.linalg.inv(F_l)
+    Finv = np.linalg.inv(F_l)
 
     return Fdet, Finv
 
-#matrices from previous assignment
+
+# matrices from previous assignment
 def localmass2D(Fdet):
-    mref=2*np.array([[1/12,1/24,1/24],[1/24,1/12,1/24],[1/24,1/24,1/12]])
-    mloc=mref*abs(Fdet)
+    mref = 2 * np.array([[1 / 12, 1 / 24, 1 / 12], [1 / 24, 1 / 12, 1 / 24], [1 / 24, 1 / 24, 1 / 12]])
+    mloc = mref * abs(Fdet)
     return mloc
-def localstiff2D(Fdet,Finv):
+
+
+def localstiff2D(Fdet, Finv):
     B_l = Finv.dot(Finv.transpose())
-    grad_phi=np.array([[-1,0,1],[-1,1,0]])
-    sloc=abs(Fdet)*np.linalg.multi_dot([grad_phi.transpose(),B_l,grad_phi])
+    grad_phi = np.array([[-1, 0, 1], [-1, 1, 0]])
+    sloc = abs(Fdet) * np.linalg.multi_dot([grad_phi.transpose(), B_l, grad_phi])
     return sloc
+
 
 if __name__ == "__main__":
     # FEM Python sample code
     x, y, npo, ne, e2, idp, ide = readtria('./meshes/disc')  # read mesh from file
-    localtoglobal2DP1 = e2                  # could it be this simple? why?
+    localtoglobal2DP1 = e2  # could it be this simple? why?
     # select points without Dirichlet bc
     it = np.logical_not(idp == 1)
     nphi = 3
 
     # build matrices
-    ii = np.zeros((ne, nphi**2))  # sparse i-index
-    jj = np.zeros((ne, nphi**2))  # sparse j-index
-    aa = np.zeros((ne, nphi**2))  # entry of Galerkin matrix
-    bb = np.zeros((ne, nphi**2))  # entry in mass-matrix (to build rhs)
+    ii = np.zeros((ne, nphi ** 2))  # sparse i-index
+    jj = np.zeros((ne, nphi ** 2))  # sparse j-index
+    aa = np.zeros((ne, nphi ** 2))  # entry of Galerkin matrix
+    bb = np.zeros((ne, nphi ** 2))  # entry in mass-matrix (to build rhs)
 
     for k in np.arange(0, ne):
         Fdet, Finv = generatetransformation2D(k, e2, x, y)  # compute trafo
 
         # build local matrices (mass, stiffness, ...)
         sloc = localstiff2D(Fdet, Finv)  # element stiffness matrix
-        mloc = localmass2D(Fdet)       # element mass matrix
+        mloc = localmass2D(Fdet)  # element mass matrix
 
         # compute i,j indices of the global matrix
         dofs = localtoglobal2DP1[k, :]
@@ -72,7 +75,7 @@ if __name__ == "__main__":
     M = sparse.csr_matrix((bb.flatten(order="F"), (ii.flatten(
         order="F"), jj.flatten(order="F"))), shape=(npo, npo))
     # build rhs and take into account Dirichlet bcs, solve, plot
-    rhs = M*np.ones(npo)
+    rhs = M * np.ones(npo)
     u = np.zeros(npo)
     u[it] = spsolve(A[np.ix_(it, it)], rhs[it])
 
@@ -81,7 +84,8 @@ if __name__ == "__main__":
     ax = fig.gca(projection='3d')
 
     # , linewidth=0.2, antialiased=True)
-    u_exact=-1/4*(x**2+y**2)+1
-    ax.plot_trisurf(x, y, u, triangles=e2)
+    u_exact = -1 / 4 * (x ** 2 + y ** 2) + 1
+    ax.plot_trisurf(x, y, u, triangles=e2, alpha=0.6)
+    ax.plot_trisurf(x, y, u_exact, alpha=0.3)
     plt.show()
-    print(sum(u-u_exact)/len(u))
+    print("Error is %f", sum(u - u_exact) / len(u))
