24d29de1cadd17e569e691d5d1bc0c1ffb5c5668
Riccardo Parise <riccardo@parise.space>
2020-01-26 17:12:57 +0100

    exercise 11 2.b)

--- Ex11/a11ex02evpb.py
+++ Ex11/a11ex02evpb.py
@@ -1,24 +1,63 @@
 import numpy as np
 from scipy import sparse
-from scipy.sparse.linalg import spsolve
+from scipy.sparse.linalg import spsolve, eigs, eigsh
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from readtria import readtria
-from elliptic2d import *
 
-def localstiff2D2(Fdet, Finv):
-    gradphi = np.array([[0, 0], [0, 0], [0, 0]])
+def generatetransformation2D(k, e2, x, y):
+    dx1 = x[e2[k, 1]]-x[e2[k, 0]]
+    dy1 = y[e2[k, 1]]-y[e2[k, 0]]
+
+    dx2 = x[e2[k, 2]]-x[e2[k, 0]]
+    dy2 = y[e2[k, 2]]-y[e2[k, 0]]
+
+    # determinant on each triangle
+    Fdet = dx1*dy2 - dx2*dy1
+
+    # transformation jacobian on each triangle
+    Finv = np.zeros((2, 2))
+    Finv[0, 0] = dy2 / Fdet
+    Finv[0, 1] = -dx2 / Fdet
+    Finv[1, 0] = -dy1 / Fdet
+    Finv[1, 1] = dx1 / Fdet
+
+    return Fdet, Finv
+
+
+def localstiff2D(Fdet, Finv):
+    gradphi = np.array([[-1, -1], [1, 0], [0, 1]])
     dphi = gradphi.dot(Finv)
     S = 1/2*(dphi[:, 0].reshape(-1, 1).dot(dphi[:, 0].reshape(1, -1)) +
              dphi[:, 1].reshape(-1, 1).dot(dphi[:, 1].reshape(1, -1)))*Fdet
     return S
 
+# Local P1 mass matrix
+
+
+def localmass2D(Fdet):
+    return Fdet*np.array([[1, 1/2, 1/2], [1/2, 1, 1/2], [1/2, 1/2, 1]])/12
+
+
 if __name__ == "__main__":
     # FEM Python sample code
     x, y, npo, ne, e2, idp, ide = readtria('mdisc')  # read mesh from file
-    localtoglobal2DP1 = e2                  # could it be this simple? why?
     # select points without Dirichlet bc
     it = np.logical_not(idp == 1)
+
+    # fake mesh
+    # x = np.array([0, 1, 1, 0, 0.5])
+    # y = np.array([0, 0, 1, 1, 0.5])
+    # e2 = np.array([[0, 1, 4], [1, 2, 4], [2, 3, 4], [3, 0, 4]])
+    # # select points without Dirichlet bc
+    # it = np.array([False, False, False, False, True])
+    # ne = 4
+    # npo = 5
+    # idp = np.array([1, 1, 1, 1, 0])
+    # ide = np.array([[1], [1], [1], [1], [1]])
+
+
+    localtoglobal2DP1 = e2                  # could it be this simple? why?
     nphi = 3
 
     # build matrices
@@ -50,15 +89,16 @@ if __name__ == "__main__":
         order="F"), jj.flatten(order="F"))), shape=(npo, npo))
 
     # build rhs and take into account Dirichlet bcs, solve, plot
-    rhs = M
+    rhs = M*np.ones(npo)
     u = np.zeros(npo)
-    # rhs = M * 1 * np.ones(npo)
-    # u[it] = spsolve(A[np.ix_(it, it)], rhs[it])
-    z = np.zeros(npo)
-    u[it] = spsolve(A[np.ix_(it, it)]-20*M[np.ix_(it, it)], z[it])
+    w, v = eigs(A[np.ix_(it, it)], M=M[np.ix_(it, it)], sigma=1)
+
     # plotting
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
-    # , linewidth=0.2, antialiased=True)
-    ax.plot_trisurf(x, y, u, triangles=e2)
+    fig, ax = plt.subplots(1, 6, figsize=(18, 4))
+    fig.suptitle('Eigenvalues')
+    for i in range(6):
+        u[it] = np.real(v[:, i])
+        ax[i].tricontourf(x, y, u, 100, cmap="inferno")
+        ax[i].title.set_text("%3.1f + %3.1fj" % (np.real(w[i]), np.real(w[i])))
+        ax[i].axis('off')
     plt.show()
